2078e0fdeb1bc4b9d8b441fa5ce39b67328c8993
SignalingServerWeb2 <jhg14734@gmail.com>
2026-06-30 02:43:24 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,9 +2,30 @@ using Microsoft.AspNetCore.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSignalR();
+builder.Services.AddSignalR(options =>
+{
+    // Allow larger image frames (10 MB)
+    options.MaximumReceiveMessageSize = 10 * 1024 * 1024;
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin();
+    });
+});
 
 var app = builder.Build();
+
+app.UseRouting();
+
+app.UseCors();
+
+app.MapGet("/", () => "SignalR Server Running");
 
 app.MapHub<SignalHub>("/signal");
 
