c20ca63c77f649dc217008dab178b4f85884d980
SignalingServerWeb2 <jhg14734@gmail.com>
2026-06-30 01:39:29 -0400

    Update SignalHub.cs

--- SignalHub.cs
+++ SignalHub.cs
@@ -2,4 +2,19 @@ using Microsoft.AspNetCore.SignalR;
 
 public class SignalHub : Hub
 {
+    public async Task RegisterUser(string userId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+    }
+
+    public async Task WatchUser(string userId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+    }
+
+    public async Task SendFrame(string userId, byte[] imageBytes)
+    {
+        await Clients.Group(userId)
+            .SendAsync("ReceiveFrame", userId, imageBytes);
+    }
 }
