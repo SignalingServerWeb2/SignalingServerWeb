4a2c9d6becd099a5f7f7cde925a9781ed19d6b86
SignalingServerWeb2 <jhg14734@gmail.com>
2026-06-30 22:52:07 -0400

    Create DeviceInfo.cs

---
+++ Models/DeviceInfo.cs
@@ -0,0 +1,15 @@
+namespace MyWebServer.Models
+{
+    public class DeviceInfo
+    {
+        public string DeviceId { get; set; } = string.Empty;
+
+        public string ComputerName { get; set; } = string.Empty;
+
+        public string ConnectionId { get; set; } = string.Empty;
+
+        public bool Online { get; set; }
+
+        public DateTime ConnectedAt { get; set; } = DateTime.UtcNow;
+    }
+}
